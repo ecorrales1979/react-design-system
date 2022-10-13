fbfed5f488cdd5fbd5c01e51f6269d1a011c3784
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-10-12 23:17:52 -0300

    feat: create tokens in tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
+    colors: {
+      "gray-900": "#121214",
+      "gray-900": "#202024",
+      "gray-900": "#7c7c8a",
+      "gray-900": "#c4c4cc",
+      "gray-900": "#e1e1e6",
+      "cyan-500": "#81d8f7",
+      "cyan-300": "#9be1fb",
+    },
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
     extend: {
       fontFamily: {
         sans: "Inter, sans-serif",
