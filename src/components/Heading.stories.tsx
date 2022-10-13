f4bf0c556fc5b5becbee16b8edc7e33f1deb1c2c
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-10-13 00:34:47 -0300

    feat: remove asChild from controls in stories

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -34,6 +34,11 @@ export const CustomComponent: StoryObj<HeadingProps> = {
       table: {
         disable: true
       }
+    },
+    asChild: {
+      table: {
+        disable: true
+      }
     }
   }
 }
--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -34,6 +34,11 @@ export const CustomComponent: StoryObj<TextProps> = {
       table: {
         disable: true
       }
+    },
+    asChild: {
+      table: {
+        disable: true
+      }
     }
   }
 }
