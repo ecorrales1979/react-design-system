d2270a446d0372272f48c1470b56ae82838e6052
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-10-16 23:02:01 -0300

    feat: add custom className to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,9 +5,10 @@ import { Slot } from '@radix-ui/react-slot';
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   asChild?: boolean;
   fullWidth?: boolean;
+  className?: string;
 }
 
-export function Button({ children, asChild, fullWidth, ...props }: ButtonProps) {
+export function Button({ children, asChild, fullWidth, className, ...props }: ButtonProps) {
   const Component = asChild ? Slot : 'button'
 
   return (
@@ -17,7 +18,8 @@ export function Button({ children, asChild, fullWidth, ...props }: ButtonProps) 
           'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm font-sans transition-colors hover:bg-cyan-300 focus:ring-2 ring-white',
           {
             'w-full': fullWidth
-          }
+          },
+          className,
         )
       }
       {...props}
